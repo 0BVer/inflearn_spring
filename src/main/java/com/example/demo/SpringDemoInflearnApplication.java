c459c68883678511595f5f414bf28b81c4fa862d
BVer <dr0joon@gmail.com>
2022-02-21 22:26:49 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/SpringDemoInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoInflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoInflearnApplication.class, args);
+	}
+
+}
