245345a4c21a263ea933db3d7256feb7f1ae9286
0BVer <dr0joon@gmail.com>
2022-03-02 17:12:06 +0900

    FEATURE : create Order Test two Version

---
+++ src/test/java/com/example/demo/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.order;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+    }
+
+}
