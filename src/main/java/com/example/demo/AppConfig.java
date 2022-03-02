d6455926fa93f2d88c4c157730323a68e61ccf5a
0BVer <dr0joon@gmail.com>
2022-03-02 20:30:41 +0900

    FEATURE : change to Spring (add Bean)

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -8,21 +8,30 @@ import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
 import com.example.demo.order.OrderService;
 import com.example.demo.order.OrderServiceImpl;
+import com.example.demo.order.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() { //여기만 바꾸면 다른 구현체로 바꿀 수 있다.
+    @Bean
+    public MemberRepository memberRepository() { //여기만 바꾸면 다른 구현체로 바꿀 수 있다.
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() { //여기만 바꾸면 다른 구현체로 바꿀 수 있다.
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); //이렇게 바꾸면 할인 정책이 바뀐다.
     }
 }
