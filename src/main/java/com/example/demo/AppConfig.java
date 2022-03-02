4ad6148c21f1fe801bfff8a8786e28821258bdce
0BVer <dr0joon@gmail.com>
2022-03-02 19:00:11 +0900

    FEATURE : refactoring AppConfig

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import com.example.demo.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() { //여기만 바꾸면 다른 구현체로 바꿀 수 있다.
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() { //여기만 바꾸면 다른 구현체로 바꿀 수 있다.
+        return new FixDiscountPolicy();
     }
 }
